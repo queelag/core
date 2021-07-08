7a93d61bab418a7474a141315249794e535d5553
Dario Sechi <dario.sechi.rnd@gmail.com>
2021-07-08 23:58:14 +0200

    Add promise utils

---
+++ src/utils/promise.utils.ts
@@ -0,0 +1,24 @@
+import { tcp } from '../modules/tcp'
+
+class PromiseUtils {
+  static async chain(...fns: ((...args: any) => Promise<any>)[]): Promise<void> {
+    for (let i = 0; i < fns.length; i++) {
+      await tcp(() => fns[i]())
+    }
+  }
+  static async truthyChain(...fns: ((...args: any) => Promise<any>)[]): Promise<boolean> {
+    let output: boolean | Error
+
+    for (let i = 0; i < fns.length; i++) {
+      output = await tcp(() => fns[i]())
+      if (output instanceof Error) return false
+
+      output = Boolean(output)
+      if (!output) return false
+    }
+
+    return true
+  }
+}
+
+export { PromiseUtils }
