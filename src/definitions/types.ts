f9521607aa088fed56e542f2f8316fff75bb868f
Dario Sechi <dario.sechi.rnd@gmail.com>
2021-09-20 20:37:16 +0200

    Remove KeyOf namespace

--- src/definitions/types.ts
+++ src/definitions/types.ts
@@ -1,18 +1,18 @@
 export type ID = string
 
-export namespace KeyOf {
-  type DeepJoin<K, P> = K extends string | number ? (P extends string | number ? `${K}${'' extends P ? '' : '.'}${P}` : never) : never
-  type DeepPrev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]]
+// export namespace KeyOf {
+//   type DeepJoin<K, P> = K extends string | number ? (P extends string | number ? `${K}${'' extends P ? '' : '.'}${P}` : never) : never
+//   type DeepPrev = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, ...0[]]
 
-  export type Deep<T, D extends number = 10> = [D] extends [never]
-    ? never
-    : T extends object
-    ? {
-        [K in keyof T]-?: K extends string | number ? `${K}` | DeepJoin<K, Deep<T[K], DeepPrev[D]>> : never
-      }[keyof T]
-    : ''
+//   export type Deep<T, D extends number = 10> = [D] extends [never]
+//     ? never
+//     : T extends object
+//     ? {
+//         [K in keyof T]-?: K extends string | number ? `${K}` | DeepJoin<K, Deep<T[K], DeepPrev[D]>> : never
+//       }[keyof T]
+//     : ''
 
-  export type Shallow<T> = keyof T
-}
+//   export type Shallow<T> = keyof T
+// }
 
 export type StatusTransformer = (keys: string[]) => string
