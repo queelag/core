e4748c53e3c51a3d2f15def8e7cb8448302ba520
Dario Sechi <dario.sechi.rnd@gmail.com>
2021-10-31 22:46:32 +0100

    Polish the addEventListenerAndReturnRemover method and add it to DocumentUtils too

--- src/utils/document.utils.ts
+++ src/utils/document.utils.ts
@@ -8,6 +8,14 @@ import { Environment } from '../modules/environment'
  */
 export class DocumentUtils {
   /**
+   * Adds an event listener and returns a function that removes it, useful with react hooks.
+   */
+  static addEventListenerAndReturnRemover(type: keyof DocumentEventMap, listener: (...args: any) => any): () => void {
+    document.addEventListener(type, listener)
+    return () => document.removeEventListener(type, listener)
+  }
+
+  /**
    * Creates a K element if the DOM is available otherwise an empty object is returned.
    */
   static createElement<K extends keyof ElementTagNameMap>(tagName: K, options?: ElementCreationOptions): ElementTagNameMap[K] {
--- src/utils/window.utils.ts
+++ src/utils/window.utils.ts
@@ -7,7 +7,7 @@ export class WindowUtils {
   /**
    * Adds an event listener and returns a function that removes it, useful with react hooks.
    */
-  static addEventListenerAndReturnRemover(type: keyof WindowEventMap, listener: () => any): () => void {
+  static addEventListenerAndReturnRemover(type: keyof WindowEventMap, listener: (...args: any) => any): () => void {
     window.addEventListener(type, listener)
     return () => window.removeEventListener(type, listener)
   }
