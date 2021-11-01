597fd472268e7777b70fd61de177610fc6a18936
Dario Sechi <dario.sechi.rnd@gmail.com>
2021-11-01 01:15:39 +0100

    Update docs

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,7 +2,7 @@ import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
-  external: ['cookie', 'encoding', 'fetch-blob', 'nanoid', 'node-fetch', 'rfc4648'],
+  external: ['cookie', 'encoding', 'fetch-blob', 'lodash/cloneDeep', 'lodash/merge', 'nanoid', 'node-fetch', 'rfc4648'],
   input: 'src/index.ts',
   output: [
     {
