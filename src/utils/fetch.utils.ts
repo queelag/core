1c223da1313fe9069d21a9a0161c553c4d4edb40
Dario Sechi <dario.sechi.rnd@gmail.com>
2022-01-24 22:50:10 +0100

    Fix merging of headers not working if the target header was undefined

--- src/utils/fetch.utils.ts
+++ src/utils/fetch.utils.ts
@@ -40,7 +40,7 @@ export class FetchUtils {
     let merged: FetchRequestInit<V>
 
     merged = ObjectUtils.merge(target, ...sources)
-    merged.headers = target.headers ? ObjectUtils.clone(target.headers) : undefined
+    merged.headers = target.headers ? ObjectUtils.clone(target.headers) : new Headers()
 
     sources.forEach((v: FetchRequestInit<V>) => {
       switch (typeof v.headers?.entries) {
@@ -62,7 +62,7 @@ export class FetchUtils {
       }
     })
 
-    if (typeof merged.headers === 'undefined') {
+    if (typeof merged.headers.entries === 'function' && [...merged.headers.entries()].length <= 0) {
       delete merged.headers
     }
 
