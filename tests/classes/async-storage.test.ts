65ab07c5ce042b66cb6af21d7f0f139c7cf9bb93
Dario Sechi <dario.sechi.rnd@gmail.com>
2025-09-28 18:10:58 +0200

    Fix async storage test

--- tests/classes/async-storage.test.ts
+++ tests/classes/async-storage.test.ts
@@ -1,123 +1,123 @@
 import { beforeEach, describe, expect, it } from 'vitest'
-import { AsyncCookie } from '../../src/classes/async-cookie'
+import { AsyncStorage } from '../../src/classes/async-storage'
 import { Configuration } from '../../src/classes/configuration'
-import { CookieItem } from '../../src/definitions/interfaces'
+import { StorageItem } from '../../src/definitions/interfaces'
 import { rne } from '../../src/functions/rne'
 import { rv } from '../../src/functions/rv'
 import { cloneObject, copyObjectProperty, setObjectProperty } from '../../src/utils/object-utils'
 
-describe('AsyncCookie', () => {
-  let map: Map<string, any>, cookie: AsyncCookie
+describe('AsyncStorage', () => {
+  let map: Map<string, any>, storage: AsyncStorage
 
   beforeEach(() => {
     map = new Map()
-    cookie = new AsyncCookie(
-      'TestCookie',
+    storage = new AsyncStorage(
+      'TestStorage',
       async () => map.clear(),
       async (key: string) => map.get(key),
       async (key: string) => map.has(key),
       async (key: string) => rv(() => map.delete(key)),
-      async (key: string, value: CookieItem) => rv(() => map.set(key, value))
+      async (key: string, value: StorageItem) => rv(() => map.set(key, value))
     )
   })
 
   it('sets and gets an item', async () => {
-    await cookie.set('person', { name: 'john' })
-    expect(await cookie.get('person')).toStrictEqual({ name: 'john' })
+    await storage.set('person', { name: 'john' })
+    expect(await storage.get('person')).toStrictEqual({ name: 'john' })
 
-    await cookie.set('person', { name: 'robert', surname: 'smith' }, ['surname'])
-    expect(await cookie.get('person')).toStrictEqual({ name: 'john', surname: 'smith' })
+    await storage.set('person', { name: 'robert', surname: 'smith' }, ['surname'])
+    expect(await storage.get('person')).toStrictEqual({ name: 'john', surname: 'smith' })
   })
 
   it('removes an item', async () => {
-    await cookie.set('person', { name: 'john', surname: 'doe' })
-    expect(await cookie.has('person')).toBeTruthy()
+    await storage.set('person', { name: 'john', surname: 'doe' })
+    expect(await storage.has('person')).toBeTruthy()
 
-    await cookie.remove('person', ['name'])
-    expect(await cookie.has('person', ['name'])).toBeFalsy()
-    expect(await cookie.has('person', ['surname'])).toBeTruthy()
+    await storage.remove('person', ['name'])
+    expect(await storage.has('person', ['name'])).toBeFalsy()
+    expect(await storage.has('person', ['surname'])).toBeTruthy()
 
-    await cookie.remove('person')
-    expect(await cookie.has('person')).toBeFalsy()
+    await storage.remove('person')
+    expect(await storage.has('person')).toBeFalsy()
   })
 
   it('copies an item to a target object', async () => {
-    let target: CookieItem
+    let target: StorageItem
 
     target = {}
-    await cookie.set('person', { name: 'john', surname: 'doe' })
+    await storage.set('person', { name: 'john', surname: 'doe' })
 
-    await cookie.copy('person', target, ['name'])
+    await storage.copy('person', target, ['name'])
     expect(target.name).toBe('john')
 
-    await cookie.copy('person', target, ['surname'])
+    await storage.copy('person', target, ['surname'])
     expect(target.surname).toBe('doe')
 
-    await cookie.set('person', { name: 'robert', surname: 'smith' })
-    await cookie.copy('person', target)
+    await storage.set('person', { name: 'robert', surname: 'smith' })
+    await storage.copy('person', target)
     expect(target.name).toBe('robert')
     expect(target.surname).toBe('smith')
   })
 
   it('clears all items', async () => {
-    await cookie.set('p1', { name: 'john' })
-    await cookie.set('p2', { name: 'robert' })
+    await storage.set('p1', { name: 'john' })
+    await storage.set('p2', { name: 'robert' })
 
-    expect(await cookie.has('p1')).toBeTruthy()
-    expect(await cookie.has('p2')).toBeTruthy()
+    expect(await storage.has('p1')).toBeTruthy()
+    expect(await storage.has('p2')).toBeTruthy()
 
-    await cookie.clear()
+    await storage.clear()
 
-    expect(await cookie.has('p1')).toBeFalsy()
-    expect(await cookie.has('p2')).toBeFalsy()
+    expect(await storage.has('p1')).toBeFalsy()
+    expect(await storage.has('p2')).toBeFalsy()
   })
 
   it('handles errors from internal methods', async () => {
-    let backup: AsyncCookie
+    let backup: AsyncStorage
 
-    backup = cloneObject(cookie)
+    backup = cloneObject(storage)
 
     Configuration.functions.tcp.log = false
 
-    setObjectProperty(cookie, '_clear', rne)
-    expect(await cookie.clear()).toBeInstanceOf(Error)
-    copyObjectProperty(backup, '_clear', cookie)
+    setObjectProperty(storage, '_clear', rne)
+    expect(await storage.clear()).toBeInstanceOf(Error)
+    copyObjectProperty(backup, '_clear', storage)
 
-    setObjectProperty(cookie, '_get', rne)
-    expect(await cookie.get('person')).toBeInstanceOf(Error)
-    copyObjectProperty(backup, '_get', cookie)
+    setObjectProperty(storage, '_get', rne)
+    expect(await storage.get('person')).toBeInstanceOf(Error)
+    copyObjectProperty(backup, '_get', storage)
 
-    setObjectProperty(cookie, '_has', rne)
-    expect(await cookie.has('person')).toBeFalsy()
-    copyObjectProperty(backup, '_has', cookie)
+    setObjectProperty(storage, '_has', rne)
+    expect(await storage.has('person')).toBeFalsy()
+    copyObjectProperty(backup, '_has', storage)
 
-    setObjectProperty(cookie, '_remove', rne)
-    expect(await cookie.remove('person')).toBeInstanceOf(Error)
-    setObjectProperty(cookie, '_set', rne)
-    expect(await cookie.remove('person', [])).toBeUndefined()
-    setObjectProperty(cookie, '_get', rne)
-    expect(await cookie.remove('person', [])).toBeUndefined()
-    copyObjectProperty(backup, '_get', cookie)
-    copyObjectProperty(backup, '_remove', cookie)
-    copyObjectProperty(backup, '_set', cookie)
+    setObjectProperty(storage, '_remove', rne)
+    expect(await storage.remove('person')).toBeInstanceOf(Error)
+    setObjectProperty(storage, '_set', rne)
+    expect(await storage.remove('person', [])).toBeUndefined()
+    setObjectProperty(storage, '_get', rne)
+    expect(await storage.remove('person', [])).toBeUndefined()
+    copyObjectProperty(backup, '_get', storage)
+    copyObjectProperty(backup, '_remove', storage)
+    copyObjectProperty(backup, '_set', storage)
 
-    setObjectProperty(cookie, '_set', rne)
-    expect(await cookie.set('person', { name: 'john' })).toBeInstanceOf(Error)
-    setObjectProperty(cookie, '_get', rne)
-    expect(await cookie.set('person', { name: 'john' }, ['name'])).toBeInstanceOf(Error)
-    copyObjectProperty(backup, '_get', cookie)
-    copyObjectProperty(backup, '_set', cookie)
+    setObjectProperty(storage, '_set', rne)
+    expect(await storage.set('person', { name: 'john' })).toBeInstanceOf(Error)
+    setObjectProperty(storage, '_get', rne)
+    expect(await storage.set('person', { name: 'john' }, ['name'])).toBeInstanceOf(Error)
+    copyObjectProperty(backup, '_get', storage)
+    copyObjectProperty(backup, '_set', storage)
 
-    setObjectProperty(cookie, '_get', rne)
-    expect(await cookie.copy('person', {})).toBeUndefined()
+    setObjectProperty(storage, '_get', rne)
+    expect(await storage.copy('person', {})).toBeUndefined()
 
-    setObjectProperty(cookie, '_has', rne)
-    expect(await cookie.has('person')).toBeFalsy()
-    setObjectProperty(cookie, '_get', rne)
-    setObjectProperty(cookie, '_has', () => true)
-    expect(await cookie.has('person', [])).toBeFalsy()
-    copyObjectProperty(backup, '_get', cookie)
-    copyObjectProperty(backup, '_has', cookie)
+    setObjectProperty(storage, '_has', rne)
+    expect(await storage.has('person')).toBeFalsy()
+    setObjectProperty(storage, '_get', rne)
+    setObjectProperty(storage, '_has', () => true)
+    expect(await storage.has('person', [])).toBeFalsy()
+    copyObjectProperty(backup, '_get', storage)
+    copyObjectProperty(backup, '_has', storage)
 
     Configuration.functions.tcp.log = true
   })
